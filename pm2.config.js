8d9cb3879921bee2b5351709f619ddb5ccbf88ff
meowlet <ngoclink13@gmail.com>
2024-10-08 20:49:44 +0700

    chore: Update pm2.config.js for MoMo payment processing
    This commit updates the `pm2.config.js` file to include the necessary configuration for MoMo payment processing. It sets the name of the application, the entry point script, and the interpreter path.

---
+++ pm2.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: "momo-payment-process", // Name of your application
+  script: "/src/index.ts", // Entry point of your application
+  interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+};
